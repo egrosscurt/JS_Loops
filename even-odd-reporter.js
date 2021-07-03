5257d624d688c10aa6371b3f4d9e516b4dcaf8a2
egrosscurt <84039948+egrosscurt@users.noreply.github.com>
2021-07-03 15:53:46 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,8 @@
+for (let i = 0; i <= 20; i++) {
+    console.log("Is ",i," even or odd?");
+    if (i % 2 == 0){
+        console.log("This is even");
+    } else {
+        console.log("This is odd");
+    }
+}
